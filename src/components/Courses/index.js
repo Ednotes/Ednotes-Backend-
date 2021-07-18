d6c44838853b07d98652b1c7adca8a85987a9bfe
bolub <abiol5202@gmail.com>
2021-07-18 22:06:38 +0100

    done for now

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 // chakra
 import {
@@ -11,36 +11,36 @@ import {
   InputGroup,
   Input,
   InputRightElement,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
 // react-router-dom
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 // components
-import SingleCourse from "./SingleCourse";
-import { PlusIcon, FilterIcon } from "../UI/Svg/Icons";
+import SingleCourse from './SingleCourse';
+import { PlusIcon, FilterIcon } from '../UI/Svg/Icons';
 
 export default function courses() {
   return (
     <>
-      <Box mt={-100} ml={-14} w="65vw">
+      <Box mt={-100} ml={-14} w='65vw'>
         {/* header */}
         <Box>
-          <Text color="#fff" fontWeight="bold" fontSize="xl" mb={2}>
+          <Text color='#fff' fontWeight='bold' fontSize='xl' mb={2}>
             Courses
           </Text>
           <Flex>
-            <InputGroup size="md" maxW="25rem" my="auto">
+            <InputGroup size='md' maxW='25rem' my='auto'>
               <Input
-                bg="#021A34"
-                pr="4.5rem"
-                type="text"
+                bg='#021A34'
+                pr='4.5rem'
+                type='text'
                 border={0}
-                color="white"
+                color='white'
                 // placeholder="Enter password"
               />
-              <InputRightElement width="5.5rem">
-                <Button borderRadius="0px 5px" fontWeight="regular">
+              <InputRightElement width='5.5rem'>
+                <Button borderRadius='0px 5px' fontWeight='regular'>
                   Search
                 </Button>
               </InputRightElement>
@@ -48,18 +48,18 @@ export default function courses() {
 
             <Spacer />
 
-            <Box my="auto">
-              <Link to="/courses/new">
+            <Box my='auto'>
+              <Link to='/courses/new'>
                 <Button
                   mx={2}
-                  w="146.88px"
+                  w='146.88px'
                   h={10}
-                  borderRadius="5px"
-                  fontSize="14px"
+                  borderRadius='5px'
+                  fontSize='14px'
                 >
                   <PlusIcon width={24} height={24} />
 
-                  <Text as="span" ml={2}>
+                  <Text as='span' ml={2}>
                     Add new
                   </Text>
                 </Button>
@@ -67,14 +67,14 @@ export default function courses() {
 
               <Button
                 mx={2}
-                w="146.88px"
+                w='146.88px'
                 h={10}
-                borderRadius="5px"
-                fontSize="14px"
+                borderRadius='5px'
+                fontSize='14px'
               >
                 <FilterIcon width={20} height={20} />
 
-                <Text as="span" ml={2}>
+                <Text as='span' ml={2}>
                   Filters
                 </Text>
               </Button>
@@ -85,31 +85,31 @@ export default function courses() {
         <Box mt={16}>
           <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={5}>
             <SingleCourse
-              title="a"
-              outline="aefa"
-              department="afa"
-              university="afafa"
+              title='a'
+              outline='aefa'
+              department='afa'
+              university='afafa'
             />
 
             <SingleCourse
-              title="a"
-              outline="aefa"
-              department="afa"
-              university="afafa"
+              title='a'
+              outline='aefa'
+              department='afa'
+              university='afafa'
             />
 
             <SingleCourse
-              title="a"
-              outline="aefa"
-              department="afa"
-              university="afafa"
+              title='a'
+              outline='aefa'
+              department='afa'
+              university='afafa'
             />
 
             <SingleCourse
-              title="a"
-              outline="aefa"
-              department="afa"
-              university="afafa"
+              title='a'
+              outline='aefa'
+              department='afa'
+              university='afafa'
             />
           </SimpleGrid>
         </Box>
