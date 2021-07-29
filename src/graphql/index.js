25ae77a4709acb69ae462a69a23fbea07e12ec58
bolub <abiol5202@gmail.com>
2021-07-29 09:03:34 +0100

    logout if access denied

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,30 +1,33 @@
-import { ApolloClient, InMemoryCache, ApolloLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { onError } from "@apollo/client/link/error";
+import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
 // import { ToastsStore } from "react-toasts";
-import { createUploadLink } from "apollo-upload-client";
-import { getToken } from "../helpers/localStorage";
+import { createUploadLink } from 'apollo-upload-client';
+import { getToken } from '../helpers/localStorage';
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: getToken() ? `Bearer ${getToken()}` : "",
+      authorization: getToken() ? `Bearer ${getToken()}` : '',
     },
   };
 });
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
-    graphQLErrors.map(({ message, locations, path }) =>
-      // ToastsStore.error(message)
-      console.log(message)
-    );
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(message);
+      if (message === 'Access Denied! Please login to continue') {
+        localStorage.removeItem('ed-toke');
+        window.location.href = '/';
+      }
+    });
 
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
-let url = "https://ednotes.herokuapp.com/graphql";
+let url = 'https://ednotes.herokuapp.com/graphql';
 
 const uploadLink = createUploadLink({ uri: url });
 
