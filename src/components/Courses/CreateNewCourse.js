77a03c73153599f73dae8558155ca340477c7a8a
bolub <abiol5202@gmail.com>
2021-07-29 09:27:27 +0100

    forgot some stuff

--- src/components/Courses/CreateNewCourse.js
+++ src/components/Courses/CreateNewCourse.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // chakra
 import { Box, Button, SimpleGrid, useToast } from '@chakra-ui/react';
 
-// // components
+// components
 import CustomFormControl from '../UI/Forms/CustomFormControl';
 import SearchableSelect from '../UI/Forms/SearchableSelect';
 import { ADD_COURSE } from '../../graphql/Mutations/Manager/Courses';
 import { GET_DEPARTMENTS } from '../../graphql/queries/Manager/Departments';
 import { useMutation } from '@apollo/client';
 
-const CreateNewCourse = () => {
+const CreateNewCourse = ({ universityData, modalDisclosure }) => {
   // chakra toast
   const toast = useToast();
 
