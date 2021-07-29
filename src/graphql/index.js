e90ec7f4327bc61081b848e6ae09c634a19238cb
bolub <abiol5202@gmail.com>
2021-07-29 09:19:07 +0100

    Anothe test

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
-// import { ToastsStore } from "react-toasts";
 import { createUploadLink } from 'apollo-upload-client';
 import { getToken } from '../helpers/localStorage';
 
@@ -14,15 +13,16 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const logoutHandler = (message) => {
+  if (message === 'Access Denied! Please login to continue') {
+    localStorage.removeItem('ed-toke');
+    window.location.href = '/';
+  }
+};
+
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
-    graphQLErrors.map(({ message, locations, path }) => {
-      console.log(message);
-      if (message === 'Access Denied! Please login to continue') {
-        localStorage.removeItem('ed-toke');
-        window.location.href = '/';
-      }
-    });
+    graphQLErrors.map(({ message, locations, path }) => logoutHandler(message));
 
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
