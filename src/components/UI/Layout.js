9d4dcbf8a430b4e80d163beb6f0468be1408314c
bolub <abiol5202@gmail.com>
2021-07-29 09:44:54 +0100

    done for now

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -28,29 +28,36 @@ const Layout = ({ children }) => {
 
   return (
     <Box>
-      <Box bg='#003049' h='10rem' color='#fff'>
+      <Box
+        d='flex'
+        flexDir='column'
+        w='100%'
+        bg='#003049'
+        h='10rem'
+        color='#fff'
+      >
         {userData && (
-          <Flex float='right' mt={8} mx={14}>
-            <Text color='#fff' my='auto' mr={2}>
-              {`${userData?.firstName} ${userData?.lastName}`}
-            </Text>
+          <Menu>
+            <MenuButton ml='auto' mt={8} mr={14}>
+              <Flex>
+                <Text color='#fff' my='auto' mr={2}>
+                  {`${userData?.firstName} ${userData?.lastName}`}
+                </Text>
 
-            <Menu>
-              <MenuButton>
                 <Avatar
                   my='auto'
                   name={`${userData?.firstName} ${userData?.lastName}`}
                   // h="30px"
                   // src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
                 />
-              </MenuButton>
-              <MenuList>
-                <MenuItem onClick={logoutHandler} color='black'>
-                  Logout
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
+              </Flex>
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={logoutHandler} color='black'>
+                Logout
+              </MenuItem>
+            </MenuList>
+          </Menu>
         )}
       </Box>
 
