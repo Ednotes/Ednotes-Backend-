d3b915fc80d5f680d4c6d5abaabfb85e0d9a9547
Ridwan Ijadunola <dareedyone@gmail.com>
2021-09-02 10:46:23 +0100

    url changed to new be

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,33 +1,33 @@
-import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { onError } from '@apollo/client/link/error';
-import { createUploadLink } from 'apollo-upload-client';
-import { getToken } from '../helpers/localStorage';
+import { ApolloClient, InMemoryCache, ApolloLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+import { createUploadLink } from "apollo-upload-client";
+import { getToken } from "../helpers/localStorage";
 
 const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      authorization: getToken() ? `Bearer ${getToken()}` : '',
-    },
-  };
+	return {
+		headers: {
+			...headers,
+			authorization: getToken() ? `Bearer ${getToken()}` : "",
+		},
+	};
 });
 
 const logoutHandler = (message) => {
-  if (message === 'Access Denied! Please login to continue') {
-    localStorage.removeItem('ed-toke');
-    window.location.href = '/';
-  }
+	if (message === "Access Denied! Please login to continue") {
+		localStorage.removeItem("ed-toke");
+		window.location.href = "/";
+	}
 };
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.map(({ message, locations, path }) => logoutHandler(message));
+	if (graphQLErrors)
+		graphQLErrors.map(({ message, locations, path }) => logoutHandler(message));
 
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+	if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
-let url = 'https://ednotes.herokuapp.com/graphql';
+let url = "https://ednotes-api.herokuapp.com/graphql";
 
 const uploadLink = createUploadLink({ uri: url });
 
@@ -37,8 +37,8 @@ const link = ApolloLink.from([errorLink, authLink, uploadLink]);
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link,
-  onError: errorLink,
-  cache,
+	link,
+	onError: errorLink,
+	cache,
 });
 export default client;
